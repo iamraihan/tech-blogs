273bc80800ec3e2991e07295c8321115659affd8
iamraihan <trueraihan@gmail.com>
2022-04-02 22:58:46 +0600

    make full blog clickable

--- src/components/BlogDetails/BlogDetails.js
+++ src/components/BlogDetails/BlogDetails.js
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { blogContext } from '../../App';
+import { FaChevronLeft } from 'react-icons/fa';
 
 const BlogDetails = () => {
     const { id } = useParams()
     const [blogs, setBlogs] = useContext(blogContext)
     // console.log(blogs);
     // const { imageURL, title, admin, blog, _id } = blogs
+    const navigate = useNavigate()
 
     const blog = blogs.find(blog => blog._id == id)
     // console.log(blogs);
@@ -15,7 +17,9 @@ const BlogDetails = () => {
             <div className='bg-sky-400 pt-20 mb-20 '>
                 <div className='ml-96 '>
                     <img className='  mt-10 ml-96 mb-0 w-[500px] p-3 shadow-md' src={blog?.imageURL} alt="" />
-
+                    <div onClick={() => navigate(-1)} className='absolute top-2 flex left-5 font-bold text-white text-2xl'>
+                        <FaChevronLeft className='mt-1 '></FaChevronLeft> <button>Back</button>
+                    </div>
                 </div>
             </div>
 
